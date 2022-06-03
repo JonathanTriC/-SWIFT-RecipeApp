201e3e12096f8e4152c5ae4804ad868e7323871b
Jonathan Tri <jonathantric@gmail.com>
2022-06-03 08:14:12 +0700

    Initial Commit

---
+++ RecipeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by Jonathan Tri Christianto on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Jonathan Tri Christianto on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
