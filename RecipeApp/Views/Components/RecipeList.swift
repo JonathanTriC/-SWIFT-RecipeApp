2a24527124e79c3da0d0cfd4f08a4de97d83d99e
Jonathan Tri <jonathantric@gmail.com>
2022-06-03 13:21:21 +0700

    adding recipe card

---
+++ RecipeApp/Views/Components/RecipeList.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeList.swift
+//  RecipeApp
+//
+//  Created by Jonathan Tri Christianto on 03/06/22.
+//
+
+import SwiftUI
+
+struct RecipeList: View {
+    var recipes: [Recipe]
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text("\(recipes.count) \(recipes.count > 1 ? "recipes" : "recipe")")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                .opacity(0.7)
+                
+                Spacer()
+            }
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 15)], spacing: 15) {
+                ForEach(recipes) { recipe in
+                    RecipeCard(recipe: recipe)
+                }
+            }
+            .padding(.top)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct RecipeList_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollView {
+            RecipeList(recipes: Recipe.all)
+        }
+    }
+}
--- RecipeApp/Views/Main/HomeView.swift
+++ RecipeApp/Views/Main/HomeView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            List(Recipe.all) { recipe in
-                Text(recipe.name)
-                    .navigationTitle("My Recipe")
+            ScrollView {
+                RecipeList(recipes: Recipe.all)
             }
+            .navigationTitle("My Recipes")
         }
         .navigationViewStyle(.stack)
     }
